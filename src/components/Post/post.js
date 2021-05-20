c9e34b182138597910d817b9ce4232956acab86f
nv404 <nvnamanvyas@gmail.com>
2021-05-20 19:09:22 +0530

    Post imager loader

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function post({
   id,
@@ -7,16 +8,20 @@ export default function post({
   caption,
   profileimage
 }) {
+  const [imageLoaded, setimageLoaded] = useState(false)
 
   return (
     <div className="w-full mx-auto my-5 px-4 py-3 bg-white rounded-lg shadow-xl tracking-wide uppercase ">
       <Link href={'/User/'+username}>
       <div className="flex items-center">
-        <img className="h-9 w-9 rounded-full cursor-pointer" src={profileimage} />
+        <img className="h-9 w-9 rounded-full cursor-pointer" src={profileimage}/>
         <p className="font-bold ml-3 cursor-pointer">{username}</p>
       </div>
       </Link>
-      <img src={imageUrl} className="rounded-xl my-2 mx-auto"/>
+      <div>
+        {imageLoaded ?  null : <div className="h-80 animate-pulse w-full rounded-xl mx-auto bg-purple-800 "></div>}
+      <img src={imageUrl} className="rounded-xl my-2 mx-auto"  onLoad={() => setimageLoaded(true)}/>
+      </div>
       <p><b>{username} :</b> {caption}</p>
     </div>
   )
